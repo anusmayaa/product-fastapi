2e1a673efc70d4776736f68a586445fa41f50675
Anusmaya Rajeesh <anusmayarajeesh@gmail.com>
2026-03-09 21:17:32 +0530

    done with databse connections and query

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
 from models import Product
 from database import session,engine
 import database_models
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
@@ -19,26 +20,61 @@ products = [
     Product(id=4, name="Table", description="A wooden table", price=199.99, quantity=20),
 ]
 
-# def init_db():
-#     db=session()
-#     for product in products:
+def get_db():
+    db=session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def init_db():
+    db=session()
+    count=db.query(database_models.Product).count()
+    if count==0:
+        for product in products: 
+            db.add(database_models.Product(**product.model_dump()))
+
+        db.commit()
+init_db()
 
 @app.get("/products/")
-def get_all_products():
-    # db=session()
-    # db.query()
-    return products
+def get_all_products(db:Session=Depends(get_db)):
+    db_products=db.query(database_models.Product).all()
+   
+    return db_products
 
 
 @app.get("/products/{product_id}")
-def get_product_by_id(product_id: int):
-    for product in products:
-        if product.id == product_id:
-            return product
-    return {"error": "Product not found"}
+def get_product_by_id(product_id: int,db:Session=Depends(get_db)):
+    db_product=db.query(database_models.Product).filter(database_models.Product.id==product_id).first()
+    if db_product:
+        return db_product
+    return "Product not found"
 
 @app.post("/products/")
-def create_product(product: Product):
-    products.append(product)
-    return {"message": "Product created successfully", "product": product}
+def create_product(product: Product,db:Session=Depends(get_db)):
+    db.add(database_models.Product(**product.model_dump()))
+    db.commit()
+    return product
     
+@app.put("/product")
+def update_product(id:int,product:Product,db:Session=Depends(get_db)):
+    db_product=db.query(database_models.Product).filter(database_models.Product.id==id).first()
+    if db_product:
+        db_product.name=product.name
+        db_product.description=product.description
+        db_product.price=product.price
+        db_product.quantity=product.quantity
+        db.commit()
+        return "Product updated"
+    else:
+        return "Product not found"
+
+@app.delete("/product")
+def delete_product(id:int,db:Session=Depends(get_db)):
+    db_product=db.query(database_models.Product).filter(database_models.Product.id==id).first()
+    if db_product:
+        db.delete(db_product)
+        db.commit()
+    else:
+        return "Product not found"
